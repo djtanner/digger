655007d99c44e51f6642d13f9764782a362af39b
djtanner <zshariat08@gmail.com>
2022-11-03 19:28:05 +0400

    added apple animation to gameover

--- src/states/game/GameOverState.lua
+++ src/states/game/GameOverState.lua
@@ -2,6 +2,15 @@ GameOverState = Class{__includes = BaseState}
 
 function GameOverState:enter(params)
     self.player = params.player
+
+    self.appleGraphic =  GameObject(GAME_OBJECT_DEFS['apple'],
+   90, 72)
+
+    self.appleGraphic.stateMachine = StateMachine {
+    ['idle'] = function() return FruitIdleState(self.appleGraphic) end
+}
+
+self.appleGraphic:changeState('idle')
 end
 
 function GameOverState:update(dt)
@@ -14,13 +23,21 @@ function GameOverState:update(dt)
     if love.keyboard.wasPressed('escape') then
         love.event.quit()
     end
+
+    self.appleGraphic:update(dt)
+
 end
 
 function GameOverState:render()
+    
+    
+    
+
    love.graphics.draw(gTextures['background'], 0, 0, 0, 
     VIRTUAL_WIDTH / gTextures['background']:getWidth(),
     VIRTUAL_HEIGHT / gTextures['background']:getHeight())
    
+    self.appleGraphic:render()
 
     love.graphics.setFont(gFonts['title'])
     love.graphics.setColor(172/255, 112/255, 136/255, 1)
@@ -29,7 +46,7 @@ function GameOverState:render()
     love.graphics.setColor(0, 0, 0, 1)
     love.graphics.setFont(gFonts['medium'])
     local apple = tostring(gStateMachine.current.player.stats["apple"])
-    love.graphics.printf(apple, 120, 80, VIRTUAL_WIDTH)
+    love.graphics.printf((":  " .. apple), 120, 80, VIRTUAL_WIDTH)
 
 
     love.graphics.setFont(gFonts['small'])
